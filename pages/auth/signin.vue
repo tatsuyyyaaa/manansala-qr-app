e21c25cf8d39305d83d72716de814e4e1696a53a
paolo <castroapaolo151@gmail.com>
2025-06-20 18:00:41 +0800

    Added Login Page

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="d-flex align-center justify-center" style="height: 100vh;">
         <v-card width="500">
-            
             <v-card-text justify="center">
-                <h1> Login Page </h1>
+                <h1 class="text-center my-6"> Login Page </h1>
                 <v-form>
-                    <v-text-field v-model="name" label="Email"></v-text-field>
+                    <v-text-field v-model="name" label="Email" prepend-icon="mdi-email" outlined></v-text-field>
 
-                    <v-text-field v-model="email" label="Password"></v-text-field>
+                    <v-text-field v-model="email" label="Password" outlined type="password" prepend-icon="mdi-lock"></v-text-field>
 
                     <v-btn color="blue" block>
-                        Signin
+                        Login
                     </v-btn>
                 </v-form>
             </v-card-text>
