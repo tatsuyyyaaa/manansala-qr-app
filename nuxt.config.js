78f7addab6eaee65086255a21e2d2b133b9ce52e
paolo <castroapaolo151@gmail.com>
2025-07-04 18:09:41 +0800

    login page

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,12 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
   ],
+  auth: {
+    // Options
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -2,20 +2,23 @@
     <div class="d-flex align-center justify-center" style="height: 100vh;">
         <v-card width="500">
             <v-card-text justify="center">
-                <h1 class="text-center my-6"> Login Page </h1>
+                <h1 class="text-center my-8"> Login Page </h1>
                 <v-form>
-                    <v-text-field v-model="name" label="Email" prepend-icon="mdi-email" outlined></v-text-field>
+                    <v-text-field v-model="name" label= "Username" rounded outlined prepend-icon="mdi-account"> </v-text-field>
 
-                    <v-text-field v-model="email" label="Password" outlined type="password" prepend-icon="mdi-lock"></v-text-field>
+                    <v-text-field v-model="email" label="Password" rounded outlined type="password"
+                        prepend-icon="mdi-lock"></v-text-field>
 
-                    <v-btn color="blue" block>
-                        Login
+                    <v-btn color="blue" rounded block>
+                        Login 
                     </v-btn>
+                    <div class="mx-3 my-3 d-flex align-center justify-center"> OR </div>
+                    <v-btn color="green" class="text-capatilized" rounded block > <v-icon left> mdi-google ></v-icon> Sign in With Google </v-btn>
                 </v-form>
             </v-card-text>
         </v-card>
     </div>
-</template>
+</template> 
 <script>
 export default {
     layout: "auth"
